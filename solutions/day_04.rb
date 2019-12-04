b03a0f156037834618cc4a0aa55707288e6bb9bf
Chad Sandeman <chadsandeman@gmail.com>
2019-12-04 20:46:12 +1300

    Added solution for day 4

--- solutions/day_04.rb
+++ solutions/day_04.rb
@@ -1,10 +1,32 @@
 require_relative "solution_helper"
 include SolutionHelper
 
-def solve_a(data = default_data_file(__FILE__))
+def array_increases_only(arr)
+  arr.each_cons(2).to_a.all? { |a, b| a < b }
 end
 
-def solve_b(data = default_data_file(__FILE__))
+def viable_password?(password)
+  pairs = password.to_s.split('').each_cons(2).to_a
+
+  pairs.all? { |a, b| a <= b } && pairs.any? { |a, b| a == b }
+end
+
+def one_standalone_pair?(password)
+  chunks = password.to_s.split('').chunk { |char| char }.to_a
+
+  chunks.any? { |char, chunk| chunk.count == 2 }
+end
+
+def solve_a(data = "246540-787419")
+  lower, upper = data.split('-').map(&:to_i)
+
+  (lower..upper).select { |i| viable_password?(i) }.count
+end
+
+def solve_b(data = "246540-787419")
+  lower, upper = data.split('-').map(&:to_i)
+
+  (lower..upper).select { |i| viable_password?(i) && one_standalone_pair?(i) }.count
 end
 
 puts "\nSolution for Part A: #{solve_a}"
